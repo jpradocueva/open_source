ec530fa35ec6b2c9b837aa99ef6c0b76ed18867c
Joaquin Prado <jprado@standardshub.io>
2022-05-18 19:09:16 -0700

    update nux.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,46 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from "@nuxt/content-theme-docs";
 
 export default theme({
+  components: [
+    { path: "./components/global" },
+    {
+      path: "~~/node_modules/@nuxt/content-theme-docs/src/components/",
+      level: 1,
+    },
+  ],
+  modules: [
+    [
+      "nuxt-youtube-subscribe-module",
+      {
+        tag: "YoutubeSubscribeButton",
+      },
+    ],
+  ],
+  target: "static",
   docs: {
-    primaryColor: '#E24F55'
-  }
-})
+    primaryColor: "#E24F55",
+  },
+  build: {
+    publicPath: "/_nuxt/",
+    babel: {
+      plugins: [["@babel/plugin-proposal-private-methods", { loose: true }]],
+    },
+  },
+
+  linkedin: {
+    partnerId: "3701497",
+    disabled: false,
+  },
+  "youtube-subscribe": {
+    tag: "YoutubeSubscribeButton",
+  },
+
+  generate: {
+    fallback: true,
+  },
+  head: {
+    title: "my first nuxt proj - main page",
+    link: [{ rel: "icon", type: "image/png", href: "./oma-logo.png" }],
+  },
+});
+
